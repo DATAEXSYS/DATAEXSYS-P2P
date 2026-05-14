303e1f38a4f707371465b57f63c0c9d12f990273
Muhammad Salman Amir <mmsa155j3@gmail.com>
2026-05-14 21:12:05 +0500

    packe setup

--- peer/shared/inc/protocol/routing/PacketCateogory.h
+++ peer/shared/inc/protocol/routing/PacketCateogory.h
@@ -6,7 +6,8 @@ enum PacketCategory : uint8_t
 {
     PACKET_CATEGORY_INVALID = 0,
 
-    PACKET_CATEGORY_REQUEST,
-    PACKET_CATEGORY_ACK,
-    PACKET_CATEGORY_NACK
+    PACKET_CATEGORY_CONTROL,   // ping, pong, heartbeat
+    PACKET_CATEGORY_ROUTING,   // DSR
+    PACKET_CATEGORY_PROOF,     // MiniPoW / TierPoW
+    PACKET_CATEGORY_SYNC       // PKC / LTD / RTC
 };
